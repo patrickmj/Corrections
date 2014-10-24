51fb382d1c887bc69990f2aef2aed8311e31cfa2
patrickmj <patrickmjchnm@gmail.com>
2014-10-24 19:38:04 -0400

    minimal show a little more info about the correction

--- views/admin/index/show.php
+++ views/admin/index/show.php
@@ -3,6 +3,14 @@ echo head(array('title' => 'correction'));
 
 ?>
 
+<p>
+<?php echo metadata($corrections_correction, 'comment'); ?>
+</p>
+
+<p>
+<?php echo metadata($corrections_correction, 'status'); ?>
+</p>
+
 <?php 
 //var_dump($corrections_correction);
 echo all_element_texts($corrections_correction);
