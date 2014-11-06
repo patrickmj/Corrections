48be11bc1aca927c90d74c2580f947abb558eaa4
patrickmj <patrickmjchnm@gmail.com>
2014-11-06 18:08:41 -0500

    localify some strings

--- views/admin/index/show.php
+++ views/admin/index/show.php
@@ -14,36 +14,31 @@ $item = $corrections_correction->getItem();
     </li>
 </ul>
 <?php endif; ?>
-
+<?php echo __(''); ?>
 <ul>
-<li>On: <?php echo link_to($item, 'show', metadata($item, array('Dublin Core', 'Title'))); ?> </li>
-<li>Added: <?php echo metadata($corrections_correction, 'added'); ?></li>
-<li>Status: <?php echo metadata($corrections_correction, 'status'); ?></li>
+<li><?php echo __('On:'); ?> <?php echo link_to($item, 'show', metadata($item, array('Dublin Core', 'Title'))); ?> </li>
+<li><?php echo __('Added:'); ?> <?php echo metadata($corrections_correction, 'added'); ?></li>
+<li><?php echo __('Status:'); ?> <?php echo metadata($corrections_correction, 'status'); ?></li>
 <?php if ($reviewed = metadata($corrections_correction, 'reviewed')): ?>
-<li>Reviewed on: <?php echo $reviewed; ?></li>
+<li><?php echo __('Reviewed on:'); ?> <?php echo $reviewed; ?></li>
 <?php endif; ?>
 <?php if ($owner = $corrections_correction->getOwner() ): ?>
-<li>From: <?php echo link_to($owner, 'show', metadata($owner, 'name')); ?></li>
+<li><?php echo __('From:'); ?> <?php echo link_to($owner, 'show', metadata($owner, 'name')); ?></li>
 <?php else: ?>
     <?php if ($corrections_correction->may_contact): ?>
-    <li>From: <a href='mailto:<?php echo metadata($corrections_correction, 'email'); ?>'><?php echo metadata($corrections_correction, 'email'); ?></a> (May Contact)</li>
+    <li><?php echo __('From:'); ?> <a href='mailto:<?php echo metadata($corrections_correction, 'email'); ?>'><?php echo metadata($corrections_correction, 'email'); ?></a> (May Contact)</li>
     <?php else: ?>
-    <li>From: <?php echo metadata($corrections_correction, 'email'); ?>(May Not Contact)</li>
+    <li><?php echo __('From:'); ?> <?php echo metadata($corrections_correction, 'email'); ?>(May Not Contact)</li>
     <?php endif; ?>
 <?php endif; ?>
-
-
 </ul>
 
 <p>
-Comment: 
+<?php echo __('Comment:'); ?>
 <?php echo metadata($corrections_correction, 'comment'); ?>
 </p>
 
 <?php 
-//var_dump($corrections_correction);
 echo all_element_texts($corrections_correction);
 ?>
-
-
 <?php echo foot(); ?>
